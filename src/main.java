2755ac2501604c8f0093caf9ed301a67b6c2c832
Matchatta Toyaem <43170683+Matchatta@users.noreply.github.com>
2021-01-11 16:24:52 +0700

    update

--- src/main.java
+++ src/main.java
@@ -1,6 +1,30 @@
-public class main {
-    public static void main(String[] args){
-        System.out.println("Hello image processing");
-        System.out.println("Test");
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Test {
+    public static void main(String[] args) {
+
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        String input = "./left_sole.jpg";
+
+        // To Read the image
+        Mat source = Imgcodecs.imread(input);
+
+        // Creating the empty destination matrix
+        Mat destination = new Mat();
+
+        // Converting the image to gray scale and
+        // saving it in the dst matrix
+        Imgproc.cvtColor(source, destination, Imgproc.COLOR_RGB2GRAY);
+
+        // Writing the image
+        Imgcodecs.imwrite("./gray.jpg", destination);
+        System.out.println("The image is successfully to Grayscale");
+
     }
 }
