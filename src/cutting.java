fb003667ab66b616e6b1d9478457b4cb5a738e3d
kamonwan-tang <https://github.com/kamonwan-tang>
2021-02-28 02:03:34 +0700

    fix cut

--- src/cutting.java
+++ src/cutting.java
@@ -1,25 +1,33 @@
 import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
-import java.awt.image.WritableRaster;
-
 import static org.opencv.core.CvType.CV_8UC1;
 
 public class cutting {
-    public Mat Cut (Mat image){
+    public Mat Cut (Mat source){
         Mat testbg = new Mat();
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         int count_black = 0;
         int count_white = 0;
-        WritableRaster wr = image.getRaster();
-        for (int y=0 ; y < image.getHeight() ; y++)
-            for (int x=0 ; x < image.getWidth() ; x++)
-                wr.getSample(x, y, 0);
-        Imgcodecs.imwrite("./testclass.jpg", image);
-        return image;
+        double ratio;
+        for (int y = 0; y < source.width(); y++) {
+            for (int x = 0; x < source.height(); x++){
+                int[] position;
+                double[] color = source.get(x, y);
+                if(color[0] == 0.0){
+                    count_black++;
+                }
+                else if(color[0] == 255.0){
+                    count_white++;
+                }
+            }
+        }
+        System.out.println(count_black);
+        System.out.println(count_white);
+        Imgcodecs.imwrite("testclass.jpg", source);
+        return source;
     }
 
 }
